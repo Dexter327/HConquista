27348db94681103145c2a1eac4600d4a2b5ef375
Dexter327 <vanderlei327@gmail.com>
2015-07-28 03:57:33 -0300

    Platform add
    Plataformas Android e Windows Phone adicionadas

---
+++ Code/platforms/wp8/www/src/js/horario.js
@@ -0,0 +1,64 @@
+(function(){
+    var app = angular.module('horario', []);
+ 
+     app.controller('controleTurno', function(){
+        this.horario = 1;
+         
+         this.setTurno = function(turno){
+            this.horario = turno;
+         };
+         
+         this.isTurno = function(tabAtivo){
+                return this.horario === tabAtivo;
+   
+         };
+         
+     });   
+
+})();
+            
+function showDia(tipo){
+    document.getElementById("local").style.display = "none";
+    document.getElementById("backEscolha").style.display = "block";
+
+    if(tipo == 1){    
+        document.getElementById("refB").style.display = "block";
+        document.getElementById("diaBairro").style.display = "block";
+    }
+
+    if(tipo == 2){
+        document.getElementById("refC").style.display = "block";
+        document.getElementById("diaCentro").style.display = "block";
+    }
+
+    if(tipo == 3){    
+        document.getElementById("refBII").style.display = "block";
+        document.getElementById("diaBairroII").style.display = "block";
+    }
+
+    if(tipo == 4){
+        document.getElementById("refCII").style.display = "block";
+        document.getElementById("diaCentroII").style.display = "block"; 
+    }
+}
+
+function dia(){
+    var newSite = document.URL;
+    newSite = newSite.split('escolha.html').pop();
+    if(newSite == '#diaBairro'){
+        showDia(1);
+    }
+    if(newSite == '#diaCentro'){
+        showDia(2);
+    }
+    if(newSite == '#diaBairroII'){
+        showDia(3);
+    }
+    if(newSite == '#diaCentroII'){
+        showDia(4);
+    }
+    if(newSite == '#buscaHorario'){
+        window.location = '../../buscaHorario.html';
+    }
+    
+}
